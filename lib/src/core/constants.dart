6b29744352874caeab28dbf74635e4061eeeb395
ale24dev <ale.24dev@gmail.com>
2024-09-24 20:42:38 -0400

    add: default padding to screens

---
+++ lib/src/core/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class Constants {
+  /// Default screen padding for all screens
+  static const EdgeInsets defaultScreenPadding =
+      EdgeInsets.symmetric(horizontal: 20);
+}
